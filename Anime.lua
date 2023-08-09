bd7371a3e84e72a6c3afefd0232a843fc1a0f160
BigJoeyJoeyFunnies <140270124+BigJoeyJoeyFunnies@users.noreply.github.com>
2023-08-09 16:17:30 -0700

    Create Anime.lua

---
+++ Anime.lua
@@ -0,0 +1,10 @@
+
+
+
+
+game.Lighting.Sky.SkyboxBk = "http://www.roblox.com/asset/?id=14330565986"
+game.Lighting.Sky.SkyboxDn = "http://www.roblox.com/asset/?id=14330586340"
+game.Lighting.Sky.SkyboxFt = "http://www.roblox.com/asset/?id=14330572603"
+game.Lighting.Sky.SkyboxLf = "http://www.roblox.com/asset/?id=14330578858"
+game.Lighting.Sky.SkyboxRt = "http://www.roblox.com/asset/?id=14330569172"
+game.Lighting.Sky.SkyboxUp = "http://www.roblox.com/asset/?id=14330582541"
