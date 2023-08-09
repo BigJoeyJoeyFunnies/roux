484f12f41c7a5ebb0e7788a2ad2da1be2b07cdf3
BigJoeyJoeyFunnies <140270124+BigJoeyJoeyFunnies@users.noreply.github.com>
2023-08-09 16:16:22 -0700

    Create PurplueSky.lua

---
+++ PurplueSky.lua
@@ -0,0 +1,7 @@
+
+game.Lighting.Sky.SkyboxBk = "http://www.roblox.com/asset/?id=14377226407"
+game.Lighting.Sky.SkyboxDn = "http://www.roblox.com/asset/?id=14377262378"
+game.Lighting.Sky.SkyboxFt = "http://www.roblox.com/asset/?id=14377234229"
+game.Lighting.Sky.SkyboxLf = "http://www.roblox.com/asset/?id=14377246718"
+game.Lighting.Sky.SkyboxRt = "http://www.roblox.com/asset/?id=14377229625"
+game.Lighting.Sky.SkyboxUp = "http://www.roblox.com/asset/?id=14377257222"
